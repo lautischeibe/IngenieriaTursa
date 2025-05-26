be5247978e5c55f3a27627712334a4f1fb2c0f5d
Lauti <lautaromflores@outlook.com>
2025-05-25 21:57:18 -0300

    Optimizaciones y edits

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,8 @@
-(function(){
+(function () {
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
-
-
+    const links = document.querySelectorAll('.nav__link a'); // Selecciona los enlaces dentro del menú
 
     openButton.addEventListener('click', () => {
         menu.classList.add('nav__link--show');
@@ -13,5 +12,10 @@
         menu.classList.remove('nav__link--show');
     });
 
-    
+    // Agregar evento a cada enlace dentro del menú
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('nav__link--show'); // Cierra el menú al hacer clic en cualquier enlace
+        });
+    });
 })();
